b175678852bde9bdf280695b2efc35cd800404c7
Ayushi <ayushi6tripathi@gmail.com>
2024-01-29 02:38:42 +0530

    EMS2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Login from "./components/login/login";
 import UserDashboard from "./components/user dashboard/userdashboard";
 import AdminDashboard from "./components/admin dashboard/admindashboard";
 import EventList from "./components/eventlist/eventlist";
-import ForgotPassword from "./components/forgotpassword/forgotpasword"
+// import ForgotPassword from "./components/forgotpassword/forgotpasword"
 import Home from "./components/home/home";
 import CreateEvent from "./components/createevent/createevent";
 // import Header from "./components/header/header";
@@ -17,7 +17,7 @@ function App() {
             <Routes>
                 <Route exact path="/" element={<Login />} />
                 <Route exact path="/signup" element={<Signup />} />
-                <Route exact path="/forgotpassword" element={<ForgotPassword />} />
+                {/* <Route exact path="/forgotpassword" element={<ForgotPassword />} /> */}
                 <Route exact path="/user" element={<UserDashboard />} />
                 <Route exact path="/admin" element={<AdminDashboard />} />
                 <Route exact path="/eventlist" element={<EventList />} />
