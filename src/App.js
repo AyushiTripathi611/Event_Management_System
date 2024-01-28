5300548d1d650556d37612f2cb78dffefe232b1f
Ayushi <ayushi6tripathi@gmail.com>
2024-01-29 03:21:27 +0530

    Hashroutertrial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Signup from "./components/signup/signup";
 import Login from "./components/login/login";
 import UserDashboard from "./components/user dashboard/userdashboard";
